1ef8b76cde71dc3b26daa3a2e652f3b715f00745
Mat-123 <156803363+Mat-123@users.noreply.github.com>
2024-05-09 17:33:01 +0200

    .

--- import.php
+++ import.php
@@ -23,7 +23,7 @@ fgetcsv($fileHandle);
 $sql = "INSERT INTO wp_users (ID, user_login, user_pass, user_nicename, user_email, user_url, user_registered, user_activation_key, user_status, display_name) VALUES (:valore1, :valore2, :valore3, :valore4, :valore5, :valore6, :valore7, :valore8, :valore9, :valore10)";
 $stmt = $pdo->prepare($sql);
 
-while (($row = fgetcsv($fileHandle)) !== false) {
+while (($row = fgetcsv($fileHandle, 0, ',')) !== false) {
     $valore1 = $row[0];
     $valore2 = $row[1];
     $valore3 = $row[2];
